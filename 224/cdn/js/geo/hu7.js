85f2370a407adc172bcd9c6e280f90426a653fd4
tea1998 <64587027+tea1998@users.noreply.github.com>
2020-05-17 19:45:30 +0300

    Add files via upload

---
+++ 224/cdn/js/geo/hu7.js
@@ -0,0 +1,20 @@
+(function() {
+    'use strict';
+
+    window.countryList = {
+        hu: {
+            name: 'Magyarország',
+            code: 'hu',
+            price: 9700,
+            oldPrice: 19400,
+            labelPrice: 'Ft',
+            phoneHelper: 'Például, +36 12345678 ',
+            nameHelper: 'Hajnalka Fruzsina',
+            nameError: 'A név kötelezően kitöltendő',
+            phoneError: 'A telefonszám kötelezően kitöltendő',
+            countryError: 'Az ország kötelezően kitöltendő',
+            isDefault: true
+        }
+    };
+    
+})();
